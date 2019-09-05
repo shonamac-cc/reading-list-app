db9f27d3005fafe7b3aa77e63523b684808a86f0
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-09-05 15:28:23 +0100

    tidy up unused code

--- js/app.js
+++ js/app.js
@@ -4,14 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleFormSubmit = function (event) {
     event.preventDefault();
     console.dir(event);
-    // const newTitleItem = document.createElement('li');
-    // const newAuthorItem = document.createElement('li');
-    // const newCategoryItem = document.createElement('li')
-    const newReadingListItem = document.createElement('ul')
+    
+    const newReadingListItem = document.createElement('li')
     const readingList = document.querySelector('#reading-list');  
     newReadingListItem.textContent = `Title: ${event.target.title.value}, Author: ${event.target.author.value}, Category: ${event.target.category.value}`
     readingList.appendChild(newReadingListItem);
-    // newReadingListItem.appendChild(newTitleItem);
+  
     form.reset();
   }
   
